b93c360b3baf761e04f3182711e1a9b1e983157b
unknown <aadityshukla64@gmail.com>
2023-10-26 03:06:51 +0530

    wiraaclone

---
+++ src/App.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+import Header from './component/Header';
+import Footer from './component/Footer';
+import First from './component/First';
+import Last from './component/Last';
+import Middle from './component/Middle';
+import Third from './component/Third';
+import Second from './component/Second';
+import First2 from './component/First2';
+import First3 from './component/First3';
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <First/>
+      <First3/>
+      <Second/>
+    
+      <First2/>
+      
+      <Third/>
+     <Middle/>
+      <Last/>
+      <Footer/>
+
+    </div>
+  );
+}
+
+export default App;
